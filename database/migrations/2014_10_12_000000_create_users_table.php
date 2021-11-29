556e4fadbcbe25deed255985888a850f1ba432a7
lesterDimps <marklesterdimapilis@gmail.com>
2021-11-29 11:37:58 +0800

    setup before merging to master file 01-29-21

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('brCode')->nullable();
+            $table->string('brCode')->default('09');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
